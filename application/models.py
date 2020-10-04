b0490801598edda480b73f0f954e1e56e9c70df9
Hazem <hazemkwita123@gmail.com>
2020-10-04 15:33:51 +0200

    add TodoList model

--- application/models.py
+++ application/models.py
@@ -1,13 +1,25 @@
 from application import db
 
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    todolist_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    todos = db.relationship('Todo', backref='todolist', lazy=True)
+
+    def __repr__(self):
+        return f'<TodoList {self.id} {self.name}>'
 
 
 lists = [
